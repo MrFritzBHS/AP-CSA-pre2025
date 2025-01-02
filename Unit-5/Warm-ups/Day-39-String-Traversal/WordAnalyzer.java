615729003b85ae24f96b0f5565ccda5c1708adcf
MrFritzBHS <fritzs@bedfordnhk12.net>
2025-01-01 20:07:28 -0500

    Create WordAnalyzer.java

---
+++ Unit-5/Warm-ups/Day-39-String-Traversal/WordAnalyzer.java
@@ -0,0 +1,28 @@
+public class WordAnalyzer {
+
+    // TODO 1 - instance variable
+    
+
+    // TODO 2 - constructors
+
+    // TODO 3 - accessor
+
+    // TODO 4 - mutator
+
+    // TODO 5 - complete the method
+    public int countAs() {
+        int count = 0;
+        for (int i = 0; i < /* missing code here */; i++) {
+            if (/* missing code here */) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // TODO 6 - make the countLetter method
+    public int countLetter(String letter) {
+        return 0;
+    }
+
+}

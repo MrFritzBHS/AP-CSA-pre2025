4872141115cbf540e9e30c0ffaeda1a456b62812
MrFritzBHS <fritzs@bedfordnhk12.net>
2025-01-01 20:42:41 -0500

    Add files via upload

---
+++ Unit-5/FRQs/Combination-Lock/CombinationLock.java
@@ -0,0 +1,8 @@
+class CombinationLock {
+
+    // what instance variables do you need?
+
+    // what should the constructor look like?
+
+    // what methods are required? public or private? void or other? parameters?
+}
--- Unit-5/FRQs/Combination-Lock/Main.java
+++ Unit-5/FRQs/Combination-Lock/Main.java
@@ -1,1 +1,11 @@
-
+class Main {
+    public static void main(String[] args) {
+        // tests DO NOT ALTER
+        CombinationLock comboLock = new CombinationLock("frog");
+        System.out.println(comboLock.getClue("oooo"));
+        System.out.println(comboLock.getClue("flip"));
+        System.out.println(comboLock.getClue("form"));
+        System.out.println(comboLock.getClue("frag"));
+        System.out.println(comboLock.getClue("frog"));
+    }
+}

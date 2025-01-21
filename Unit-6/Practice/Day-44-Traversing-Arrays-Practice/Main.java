acd8b714141ad7be3d8227ef91890ea17e1a9871
MrFritzBHS <fritzs@bedfordnhk12.net>
2025-01-21 10:53:08 -0500

    Create Main.java

---
+++ Unit-6/Practice/Day-44-Traversing-Arrays-Practice/Main.java
@@ -0,0 +1,22 @@
+class Main {
+    public static void main(String[] args) {
+        // write code to test your methods here
+        
+    }
+
+    public static String challenge1(String[] arr, String letter) {
+        return "";
+    }
+
+    public static int[] challenge2(int[] grades) {
+        return new int[] {}; // change this
+    }
+
+    public static String[] challenge3(String[] arr) {
+        return new String[] {}; // change this
+    }
+
+    public static String[] challenge4(String[] arr1, String[] arr2) {
+        return new String[] {}; // change this
+    }
+}

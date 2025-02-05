371e38c612567f550076bda09e52ed0b16413a64
MrFritzBHS <fritzs@bedfordnhk12.net>
2025-02-05 09:43:24 -0500

    Create Main.java

---
+++ Unit-7/Explorations/Day-49-ArrayList-Basics/Main.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+class Main {
+    public static void main(String[] args) {
+        theBasics();
+        basketballTasks();
+    }
+
+    public static void theBasics() {
+        
+    }
+
+    public static void basketballTasks() {
+        
+
+        /* 
+            TASK 1: add four more teams (if you need suggestions, look below)
+                "Los Angeles", "Lakers", "West"
+                "Golden State", "Warriors", "West"
+                "Philadelphia", "76ers", "East"
+                "New York City", "Knicks", "East"
+        */
+
+
+        /*
+            TASK 2: insert a team in the 3rd spot
+                "San Antonio", "Spurs", "West"
+
+        */
+
+
+        /*
+            TASK 3: replace the 5th team with
+                "Washington", "Wizards", "East"
+        */
+
+        /*
+            TASK 4: make an index-based loop to traverse the ArrayList. 
+                - Print each NBATeam Object in the "East" conference
+        */
+
+        /*
+            TASK 5 (Challenge): Remove all NBATeams in the "West" conference
+        */ 
+        
+        /*
+            TASK 6: Print out all remaining teams with an
+                enhanced for loop
+        */
+    }
+}

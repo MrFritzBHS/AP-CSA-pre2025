f81c22f4457629dd9cb5d9932573aafef7c3e1d1
MrFritzBHS <fritzs@bedfordnhk12.net>
2025-02-07 09:10:30 -0500

    Update Main.java

--- Unit-7/FRQ-Practice/Day-50-NumberTile-FRQ/Main.java
+++ Unit-7/FRQ-Practice/Day-50-NumberTile-FRQ/Main.java
@@ -1,6 +1,37 @@
 class Main {
     public static void main(String[] args) {
-        // Test your solution here by creating TileGame boards and using the samples from the FRQ document
 
+        /* Test with FRQ sample */
+        int[][] testTiles = {
+            {4, 3, 7, 4},
+            {6, 4, 3, 3},
+            {1, 2, 3, 4},
+            {3, 2, 5, 2},
+            {5, 9, 2, 2}
+        };
+        
+        TileGame testGame = new TileGame();
+        for (int[] nums : testTiles) {
+            testGame.addTile(
+                new NumberTile(nums[0], nums[1], nums[2], nums[3])
+            );
+        }
+
+        NumberTile newTile = new NumberTile(4, 2, 9, 2);
+        System.out.println(testGame.insertTile(newTile));
+        for (NumberTile tile : testGame.getBoard()) {
+            System.out.println(tile);
+        }
+
+        /* Test can't add tile */
+        newTile = new NumberTile(0, 0, 0, 0);
+        System.out.println(testGame.insertTile(newTile));
+
+        /* Test empty board */
+        testGame = new TileGame();
+        testGame.insertTile(
+            new NumberTile(1, 2, 3, 4)
+        );
+        System.out.println(testGame.getBoard());
     }
 }

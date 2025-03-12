ae43dfba60c376dacca48c73e110c377b73d1cfc
MrFritzBHS <fritzs@bedfordnhk12.net>
2025-03-12 16:45:04 -0400

    Create MagicSquareChecker.java

---
+++ Unit-8/Warm-Ups/Day-58-Magic-Square-Checker/MagicSquareChecker.java
@@ -0,0 +1,28 @@
+public class MagicSquareChecker {
+    // TODO 1 - write the getMagicNumber method
+    private static int getMagicNumber(int[][] square) {
+        
+        return 0;
+    }
+    
+    // TODO 2 - write checkRow method
+    private static boolean checkRow(int[][] square, int rowIndex) {
+        
+        return false;
+    }
+    
+    // TODO 3 - write checkColumn method
+    
+    
+    // TODO 4 - write checkDiagonals method
+    
+    
+    // TODO 5 - write isMagicSquare method
+    
+    
+    // TODO 6 (challenge) - write allNumbersUsed method
+        // and implement in isMagicSquare
+    
+    
+    
+}

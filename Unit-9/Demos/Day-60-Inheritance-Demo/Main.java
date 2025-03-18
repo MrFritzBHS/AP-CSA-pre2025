497a496ccac294374a5c1790ca24e297b229279b
MrFritzBHS <fritzs@bedfordnhk12.net>
2025-03-18 10:39:24 -0400

    Create Main.java

---
+++ Unit-9/Demos/Day-60-Inheritance-Demo/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Create a Car object
+
+        // Create a Bike object
+
+        // Create a Tesla object
+        
+        // Create a Vehicle object
+      
+    }
+}

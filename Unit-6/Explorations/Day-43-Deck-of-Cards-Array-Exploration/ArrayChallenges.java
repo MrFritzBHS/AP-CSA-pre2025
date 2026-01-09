b609513fd363be726a26d7a96058218b61b2f5ab
Mr. Fritz <fritzs@bedfordnhk12.net>
2026-01-09 14:45:56 -0500

    Create ArrayChallenges class with placeholder methods
    Added ArrayChallenges class with methods for array operations.

---
+++ Unit-6/Explorations/Day-43-Deck-of-Cards-Array-Exploration/ArrayChallenges.java
@@ -0,0 +1,33 @@
+public class ArrayChallenges {
+    
+    /* 
+        test each of these out in the main method 
+        of the Main class as you complete them
+    */
+    
+    public static int[] random500() {
+        
+        
+        return new int[]{1, 2, 3}; // change this
+    }
+    
+    public static double getArrayAverage(int[] arr) {
+        
+        return 0.0; // change this
+    }
+    
+    public static int getMinValue(int[] arr) {
+        
+        return 0; // change this
+    }
+    
+    public static int getIndexOfMax(int[] arr) {
+        
+        return 0; // change this
+    }
+    
+    public static String getLongestWord(String[] arr) {
+        
+        return ""; // change this
+    }
+}
